d89dcf7092983b0ab43c75d57b178709c502a9d1
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-06-10 12:49:39 +0200

    Remove Django example
    Superseded by openshift/django-ex.

--- 3.3/examples/django/demo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^notes/', include('notes.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-]
--- 3.3/examples/django/notes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Note
-
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('text', 'created_at', 'updated_at')
-
-admin.site.register(Note, NoteAdmin)
