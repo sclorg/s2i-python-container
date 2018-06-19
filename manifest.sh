1636bdfd3adeaabcf25523b2776486f6882eb62e
Petr Kubat <pkubat@redhat.com>
2018-06-19 13:25:34 +0200

    Generate actual test files instead of symlinks

--- manifest.sh
+++ manifest.sh
@@ -41,46 +41,7 @@ DISTGEN_MULTI_RULES="
 "
 
 # Symbolic links
-SYMLINK_RULES="
-    link_target=../../examples/app-home-test-app
-    link_name=test/app-home-test-app;
-
-    link_target=../../examples/django-test-app
-    link_name=test/django-test-app;
-
-    link_target=../../examples/locale-test-app
-    link_name=test/locale-test-app;
-
-    link_target=../../examples/mod-wsgi-test-app
-    link_name=test/mod-wsgi-test-app;
-
-    link_target=../../examples/npm-virtualenv-uwsgi-test-app
-    link_name=test/npm-virtualenv-uwsgi-test-app;
-
-    link_target=../../examples/numpy-test-app
-    link_name=test/numpy-test-app;
-
-    link_target=../../examples/setup-requirements-test-app
-    link_name=test/setup-requirements-test-app;
-
-    link_target=../../examples/setup-test-app
-    link_name=test/setup-test-app;
-
-    link_target=../../examples/standalone-test-app
-    link_name=test/standalone-test-app;
-
-    link_target=../../test/run
-    link_name=test/run;
-
-    link_target=../../test/run-openshift
-    link_name=test/run-openshift;
-
-    link_target=../../common/test-lib.sh
-    link_name=test/test-lib.sh;
-
-    link_target=../../common/test-lib-openshift.sh
-    link_name=test/test-lib-openshift.sh;
-"
+SYMLINK_RULES=""
 
 # Files to copy
 COPY_RULES="
@@ -94,15 +55,128 @@ COPY_RULES="
     dest=s2i/bin/run
     mode=0755;
 
+    src=test/run
+    dest=test/run
+    mode=0755;
+
+    src=test/run-openshift
+    dest=test/run-openshift
+    mode=0755;
+
+    src=common/test-lib.sh
+    dest=test/test-lib.sh;
+
+    src=common/test-lib-openshift.sh
+    dest=test/test-lib-openshift.sh;
+
+    src=examples/app-home-test-app/project/__init__.py
+    dest=test/app-home-test-app/project/__init__.py;
+
+    src=examples/app-home-test-app/project/wsgi.py
+    dest=test/app-home-test-app/project/wsgi.py;
+
+    src=examples/app-home-test-app/.s2i/environment
+    dest=test/app-home-test-app/.s2i/environment;
+
+    src=examples/app-home-test-app/requirements.txt
+    dest=test/app-home-test-app/requirements.txt;
+
+    src=examples/mod-wsgi-test-app/app.py
+    dest=test/mod-wsgi-test-app/app.py;
+
+    src=examples/mod-wsgi-test-app/requirements.txt
+    dest=test/mod-wsgi-test-app/requirements.txt;
+
+    src=examples/mod-wsgi-test-app/wsgi.py
+    dest=test/mod-wsgi-test-app/wsgi.py;
+
+    src=examples/pipenv-test-app/.gitignore
+    dest=test/pipenv-test-app/.gitignore;
+
     src=examples/pipenv-test-app/testapp.py
     dest=test/pipenv-test-app/testapp.py;
-
-    src=examples/pipenv-test-app/setup.py
-    dest=test/pipenv-test-app/setup.py;
 
     src=examples/pipenv-test-app/.s2i/environment
     dest=test/pipenv-test-app/.s2i/environment;
 
-    src=examples/pipenv-test-app/.gitignore
-    dest=test/pipenv-test-app/.gitignore;
+    src=examples/pipenv-test-app/setup.py
+    dest=test/pipenv-test-app/setup.py;
+
+    src=examples/django-postgresql.json
+    dest=test/django-postgresql.json;
+
+    src=examples/django-test-app/.gitignore
+    dest=test/django-test-app/.gitignore;
+
+    src=examples/django-test-app/project/settings.py
+    dest=test/django-test-app/project/settings.py;
+
+    src=examples/django-test-app/project/__init__.py
+    dest=test/django-test-app/project/__init__.py;
+
+    src=examples/django-test-app/project/urls.py
+    dest=test/django-test-app/project/urls.py;
+
+    src=examples/django-test-app/project/wsgi.py
+    dest=test/django-test-app/project/wsgi.py;
+
+    src=examples/django-test-app/requirements.txt
+    dest=test/django-test-app/requirements.txt;
+
+    src=examples/django-test-app/manage.py
+    dest=test/django-test-app/manage.py;
+
+    src=examples/standalone-test-app/app.py
+    dest=test/standalone-test-app/app.py;
+
+    src=examples/standalone-test-app/requirements.txt
+    dest=test/standalone-test-app/requirements.txt;
+
+    src=examples/django-postgresql-persistent.json
+    dest=test/django-postgresql-persistent.json;
+
+    src=examples/locale-test-app/requirements.txt
+    dest=test/locale-test-app/requirements.txt;
+
+    src=examples/locale-test-app/wsgi.py
+    dest=test/locale-test-app/wsgi.py;
+
+    src=examples/numpy-test-app/requirements.txt
+    dest=test/numpy-test-app/requirements.txt;
+
+    src=examples/numpy-test-app/wsgi.py
+    dest=test/numpy-test-app/wsgi.py;
+
+    src=examples/setup-test-app/.gitignore
+    dest=test/setup-test-app/.gitignore;
+
+    src=examples/setup-test-app/testapp.py
+    dest=test/setup-test-app/testapp.py;
+
+    src=examples/setup-test-app/setup.py
+    dest=test/setup-test-app/setup.py;
+
+    src=examples/setup-requirements-test-app/.gitignore
+    dest=test/setup-requirements-test-app/.gitignore;
+
+    src=examples/setup-requirements-test-app/testapp.py
+    dest=test/setup-requirements-test-app/testapp.py;
+
+    src=examples/setup-requirements-test-app/setup.py
+    dest=test/setup-requirements-test-app/setup.py;
+
+    src=examples/setup-requirements-test-app/requirements.txt
+    dest=test/setup-requirements-test-app/requirements.txt;
+
+    src=examples/npm-virtualenv-uwsgi-test-app/.s2i/environment
+    dest=test/npm-virtualenv-uwsgi-test-app/.s2i/environment;
+
+    src=examples/npm-virtualenv-uwsgi-test-app/app.sh
+    dest=test/npm-virtualenv-uwsgi-test-app/app.sh;
+
+    src=examples/npm-virtualenv-uwsgi-test-app/requirements.txt
+    dest=test/npm-virtualenv-uwsgi-test-app/requirements.txt;
+
+    src=examples/npm-virtualenv-uwsgi-test-app/wsgi.py
+    dest=test/npm-virtualenv-uwsgi-test-app/wsgi.py;
 "
