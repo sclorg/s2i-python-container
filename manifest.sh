2d2146cf7367e60ba06dcec18fd644dfea0e926d
Lumir Balhar <lbalhar@redhat.com>
2020-05-07 10:20:10 +0200

    fixup! Implement possitive and negative tests for micropipenv

--- manifest.sh
+++ manifest.sh
@@ -85,8 +85,8 @@ SYMLINK_RULES="
     link_target=../../examples/standalone-test-app
     link_name=test/standalone-test-app;
 
-    link_target=../../src/test/pipenv-and-micropipenv-fails-test-app
-    link_name=test/pipenv-and-micropipenv-fails-test-app;
+    link_target=../../src/test/pipenv-and-micropipenv-should-fail-test-app
+    link_name=test/pipenv-and-micropipenv-should-fail-test-app;
 
     link_target=../../test/run-openshift
     link_name=test/run-openshift;
