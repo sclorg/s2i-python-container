dc96e904393b6db42fe8ff2044d470e3e614f734
Lumir Balhar <lbalhar@redhat.com>
2021-07-28 11:05:35 +0200

    Copy one test so we can switch it to pyuwsgi

---
+++ 3.9-minimal/test/pin-pipenv-version-test-app/app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Testing PIN_PIPENV_VERSION (set in .s2i/environment) ..."
+pipenv_version=`pipenv --version`
+expected="pipenv, version 2018.11.26"
+if [ "$pipenv_version" != "$expected" ]; then
+  echo "ERROR: pipenv version is different than expected."
+  echo "Expected: ${expected}"
+  echo "Actual: ${pipenv_version}"
+  exit 1
+fi
+
+# Test the uwsgi server
+exec uwsgi \
+    --http-socket :8080 \
+    --die-on-term \
+    --master \
+    --single-interpreter \
+    --enable-threads \
+    --threads=5 \
+    --thunder-lock \
+    --module wsgi
