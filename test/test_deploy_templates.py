4c1a2a1240463e2390edd6fe7025b6af7f356bca
Petr "Stone" Hracek <phracek@redhat.com>
2024-07-19 16:18:23 +0200

    Use VERSION instead of SINGLE_VERSION.
    VERSION is used in our whole sclorg organization.
    Like here: https://github.com/sclorg/container-common-scripts/blob/master/test.sh#L81
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_deploy_templates.py
+++ test/test_deploy_templates.py
@@ -7,11 +7,11 @@ from container_ci_suite.openshift import OpenShiftAPI
 from container_ci_suite.utils import check_variables
 
 if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, SINGLE_VERSION is missing.")
+    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
     sys.exit(1)
 
 
-VERSION = os.getenv("SINGLE_VERSION")
+VERSION = os.getenv("VERSION")
 IMAGE_NAME = os.getenv("IMAGE_NAME")
 OS = os.getenv("TARGET")
 
