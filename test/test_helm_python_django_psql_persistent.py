5661df4d19f9c321a9400f679c854eeba7f20173
Petr "Stone" Hracek <phracek@redhat.com>
2024-09-13 14:38:20 +0200

    Testing files for testing helm charts in s2i-python-container
    Helm charts are stored in repository:
    https://github.com/sclorg/helm-charts
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_helm_python_django_psql_persistent.py
@@ -0,0 +1,90 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmPythonDjangoPsqlTemplate:
+
+    def setup_method(self):
+        package_name = "django-psql-persistent"
+        path = test_dir
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
+        self.hc_api.clone_helm_chart_repo(
+            repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
+            subdir="charts/redhat"
+        )
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,branch",
+        [
+            ("3.12-ubi9", "4.2.x"),
+            ("3.12-ubi8", "4.2.x"),
+            ("3.11-ubi9", "4.2.x"),
+            ("3.11-ubi8", "4.2.x"),
+            ("3.9-ubi9", "master"),
+            ("3.9-ubi8", "master"),
+            ("3.6-ubi8", "master"),
+        ],
+    )
+    def test_django_psql_curl_output(self, version, branch):
+        if self.hc_api.oc_api.shared_cluster:
+            pytest.skip("Do NOT test on shared cluster")
+        self.hc_api.package_name = "postgresql-imagestreams"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "python-imagestreams"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "django-psql-persistent"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(
+            values={
+                "python_version": version,
+                "namespace": self.hc_api.namespace,
+                "source_repository_ref": branch,
+            }
+        )
+        assert self.hc_api.is_s2i_pod_running(pod_name_prefix="django-psql")
+        assert self.hc_api.test_helm_curl_output(
+            route_name="django-psql",
+            expected_str="Welcome to your Django application"
+        )
+
+    @pytest.mark.parametrize(
+        "version,branch",
+        [
+            ("3.12-ubi9", "4.2.x"),
+            ("3.12-ubi8", "4.2.x"),
+            ("3.11-ubi9", "4.2.x"),
+            ("3.11-ubi8", "4.2.x"),
+            ("3.9-ubi9", "master"),
+            ("3.9-ubi8", "master"),
+            ("3.6-ubi8", "master"),
+        ],
+    )
+    def test_django_psql_helm_test(self, version, branch):
+        self.hc_api.package_name = "postgresql-imagestreams"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "python-imagestreams"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "django-psql-persistent"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(
+            values={
+                "python_version": version,
+                "namespace": self.hc_api.namespace,
+                "source_repository_ref": branch,
+            }
+        )
+        assert self.hc_api.is_s2i_pod_running(pod_name_prefix="django-psql")
+        assert self.hc_api.test_helm_chart(expected_str=["Welcome to your Django application"])
---
+++ test/test_helm_python_imagestreams.py
@@ -0,0 +1,40 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmRHELPythonImageStreams:
+
+    def setup_method(self):
+        package_name = "python-imagestreams"
+        path = test_dir
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
+        self.hc_api.clone_helm_chart_repo(
+            repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
+            subdir="charts/redhat"
+        )
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("3.12-ubi9", "registry.redhat.io/ubi9/python-312:latest"),
+            ("3.12-ubi8", "registry.redhat.io/ubi8/python-312:latest"),
+            ("3.11-ubi9", "registry.redhat.io/ubi9/python-311:latest"),
+            ("3.11-ubi8", "registry.redhat.io/ubi8/python-311:latest"),
+            ("3.9-ubi9", "registry.redhat.io/ubi9/python-39:latest"),
+            ("3.9-ubi8", "registry.redhat.io/ubi8/python-39:latest"),
+            ("3.6-ubi8", "registry.redhat.io/ubi8/python-36:latest"),
+        ],
+    )
+    def test_package_imagestream(self, version, registry):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
