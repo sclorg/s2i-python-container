1310e2becd1b9959ca9b74d0c19a83f32d6fdf8b
Lumir Balhar <lbalhar@redhat.com>
2025-10-03 12:05:20 +0200

    Improve handling of shared variables and don't repeate code

--- test/test_python_ex_standalone.py
+++ test/test_python_ex_standalone.py
@@ -4,31 +4,23 @@ import sys
 import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
-from container_ci_suite.utils import check_variables
 
-if not check_variables():
-    print("At least one variable from IMAGE_NAME, OS, VERSION is missing.")
-    sys.exit(1)
-
-VERSION = os.getenv("VERSION")
-IMAGE_NAME = os.getenv("IMAGE_NAME")
-OS = os.getenv("TARGET").lower()
-SHORT_VERSION = VERSION.replace(".", "")
+from conftest import VARS
 
 
 # Replacement with 'test_python_s2i_app_ex_standalone'
 class TestPythonExTemplate:
 
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix=f"python-{SHORT_VERSION}-test", version=VERSION, shared_cluster=True)
+        self.oc_api = OpenShiftAPI(pod_name_prefix=f"python-{VARS.SHORT_VERSION}-test", version=VARS.VERSION, shared_cluster=True)
 
     def teardown_method(self):
         self.oc_api.delete_project()
 
     def test_python_ex_template_inside_cluster(self):
-        service_name = f"python-{SHORT_VERSION}-test"
+        service_name = f"python-{VARS.SHORT_VERSION}-test"
         assert self.oc_api.deploy_s2i_app(
-            image_name=IMAGE_NAME, app="https://github.com/sclorg/s2i-python-container.git",
+            image_name=VARS.IMAGE_NAME, app="https://github.com/sclorg/s2i-python-container.git",
             context="examples/standalone-test-app",
             service_name=service_name
         )
